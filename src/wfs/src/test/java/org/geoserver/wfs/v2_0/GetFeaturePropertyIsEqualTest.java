0e95bb4eaf34013c359e601d304914cb311b85f2
Roar Brænden <roar.brenden.no@gmail.com>
2025-04-01 21:56:25 +0200

    Running OK with WFS 2.0

--- src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePropertyIsEqualTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePropertyIsEqualTest.java
@@ -1,4 +1,4 @@
-/* (c) 2024 Open Source Geospatial Foundation - all rights reserved
+/* (c) 2025 Open Source Geospatial Foundation - all rights reserved
  * This code is licensed under the GPL 2.0 license, available at the root
  * application directory.
  */
@@ -15,59 +15,43 @@ import org.w3c.dom.NodeList;
 
 public class GetFeaturePropertyIsEqualTest extends WFS20TestSupport {
 
+    private final String PREFIX = "app";
+    private final String URI = "http://kartverket.no/";
+
     @Override
     protected void setUpInternal(SystemTestData data) throws Exception {
 
         data.addVectorLayer(
-                new QName(SystemTestData.DEFAULT_URI, "Fylker", SystemTestData.DEFAULT_PREFIX),
-                Collections.emptyMap(),
-                "fylke.properties",
-                getClass(),
-                getCatalog());
+                new QName(URI, "Fylker", PREFIX), Collections.emptyMap(), "fylke.properties", getClass(), getCatalog());
     }
 
     @Test
-    public void testGetFeatureFilterValueReferenceLeadingZero() throws Exception {
-        testCall("ValueReference", "03");
+    public void testGetFeatureFilterLeadingZero() throws Exception {
+        testCall("03");
     }
 
     @Test
-    public void testGetFeatureFilterValueReference() throws Exception {
-        testCall("ValueReference", "32");
+    public void testGetFeatureFilter() throws Exception {
+        testCall("32");
     }
 
-    @Test
-    public void testGetFeatureFilterPropertyNameLeadingZero() throws Exception {
-        testCall("PropertyName", "03");
-    }
-
-    @Test
-    public void testGetFeatureFilterPropertyName() throws Exception {
-        testCall("PropertyName", "32");
-    }
-
-    private void testCall(String element, String value) throws Exception {
-        final String typeName = "Fylker";
-
-        String xml =
-                "<GetFeature service=\"WFS\" version=\"2.0.0\" outputFormat=\"text/xml; subtype=gml/3.2\">"
-                        + "<Query typeNames=\""
-                        + typeName
-                        + "\">"
-                        + "<Filter>"
-                        + "<PropertyIsEqualTo>"
-                        + "<"
-                        + element
-                        + ">nummer</"
-                        + element
-                        + ">"
-                        + "<Literal>"
-                        + value
-                        + "</Literal>"
-                        + "</PropertyIsEqualTo>"
-                        + "</Filter>"
-                        + "</Query>"
-                        + "</GetFeature>";
+    private void testCall(String value) throws Exception {
+        final String typeName = PREFIX + ":Fylker";
+        String xml = "<GetFeature service=\"WFS\" version=\"2.0.0\" xmlns:" + PREFIX + "=\"" + URI + "\""
+                + " xmlns:fes=\"http://www.opengis.net/fes/2.0\" outputFormat=\"text/xml; subtype=gml/3.2\">"
+                + "<Query typeNames=\""
+                + typeName
+                + "\">"
+                + "<fes:Filter>"
+                + "<fes:PropertyIsEqualTo>"
+                + "<fes:ValueReference>nummer</fes:ValueReference>"
+                + "<fes:Literal>"
+                + value
+                + "</fes:Literal>"
+                + "</fes:PropertyIsEqualTo>"
+                + "</fes:Filter>"
+                + "</Query>"
+                + "</GetFeature>";
         Document doc = postAsDOM("wfs", xml);
 
         NodeList features = doc.getElementsByTagName("wfs:member");
