c32b3a070eceee5bbf99c30e99fafa571d214f36
Roar Brænden <roar.brenden.no@gmail.com>
2025-04-02 06:40:35 +0200

    Added test for OGC filter gives ExceptionReport

--- src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePropertyIsEqualTest.java
+++ src/wfs/src/test/java/org/geoserver/wfs/v2_0/GetFeaturePropertyIsEqualTest.java
@@ -11,6 +11,7 @@ import javax.xml.namespace.QName;
 import org.geoserver.data.test.SystemTestData;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class GetFeaturePropertyIsEqualTest extends WFS20TestSupport {
@@ -27,15 +28,29 @@ public class GetFeaturePropertyIsEqualTest extends WFS20TestSupport {
 
     @Test
     public void testGetFeatureFilterLeadingZero() throws Exception {
-        testCall("03");
+        Document doc = testCallStrict("03");
+
+        NodeList features = doc.getElementsByTagName("wfs:member");
+        assertEquals(1, features.getLength());
     }
 
     @Test
     public void testGetFeatureFilter() throws Exception {
-        testCall("32");
+        Document doc = testCallStrict("32");
+
+        NodeList features = doc.getElementsByTagName("wfs:member");
+        assertEquals(1, features.getLength());
     }
 
-    private void testCall(String value) throws Exception {
+    @Test
+    public void testGetFeatureWithOgcFilter() throws Exception {
+        Document doc = testCallOgcFilter("32");
+
+        Node root = doc.getFirstChild();
+        assertEquals("ExceptionReport", root.getLocalName());
+    }
+
+    private Document testCallStrict(String value) throws Exception {
         final String typeName = PREFIX + ":Fylker";
         String xml = "<GetFeature service=\"WFS\" version=\"2.0.0\" xmlns:" + PREFIX + "=\"" + URI + "\""
                 + " xmlns:fes=\"http://www.opengis.net/fes/2.0\" outputFormat=\"text/xml; subtype=gml/3.2\">"
@@ -52,9 +67,26 @@ public class GetFeaturePropertyIsEqualTest extends WFS20TestSupport {
                 + "</fes:Filter>"
                 + "</Query>"
                 + "</GetFeature>";
-        Document doc = postAsDOM("wfs", xml);
+        return postAsDOM("wfs", xml);
+    }
 
-        NodeList features = doc.getElementsByTagName("wfs:member");
-        assertEquals(1, features.getLength());
+    private Document testCallOgcFilter(String value) throws Exception {
+        final String typeName = PREFIX + ":Fylker";
+        String xml = "<GetFeature service=\"WFS\" version=\"2.0.0\" xmlns:" + PREFIX + "=\"" + URI + "\""
+                + " xmlns:ogc=\"http://www.opengis.net/ogc\" outputFormat=\"text/xml; subtype=gml/3.2\">"
+                + "<Query typeNames=\""
+                + typeName
+                + "\">"
+                + "<ogc:Filter>"
+                + "<ogc:PropertyIsEqualTo>"
+                + "<ogc:PropertyName>nummer</ogc:PropertyName>"
+                + "<ogc:Literal>"
+                + value
+                + "</ogc:Literal>"
+                + "</ogc:PropertyIsEqualTo>"
+                + "</ogc:Filter>"
+                + "</Query>"
+                + "</GetFeature>";
+        return postAsDOM("wfs", xml);
     }
 }
